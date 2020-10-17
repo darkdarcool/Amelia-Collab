2556f0192994b91afab43ba1df387bb610849a78
Darren Reddy <66882633+darkdarcool@users.noreply.github.com>
2020-10-17 11:20:32 -0700

    Update main.py
    Did a couple bug fixes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 import os,random
 import time, sys
 def typewriter(message):
@@ -31,30 +32,44 @@ typewriter('What is your name?\n')
 name = input('       >>>  ')
 
 ### Prep done, type work now!!!
-
+os.system('clear')
 typewriter(green + 'Hi! I\'m zoom bot! I\'m an alogrithm designed for your specific needs!! I think that your name is ' + bwhite + name + green +  '! Now, I know I\'m having a good day, but are you??')
+time.sleep(2)
+os.system('clear')
 typewriter('Let\'s talk commands. I am currently in Beta, so I\'m still learning, but I have a current list of them! Type eList in the console to see the list! When asking a question, make sure you spell EVERY word right, so I can understand what you are saying a little bit better.')
 typewriter('That is enough of that, let\'s get started!')
 time.sleep(2)
 os.system('clear')
 print('Ask me anything!\n')
 a = input(green + '')
-if a == "eList":
-  os.system('clear')
-  typewriter('Here are the commands!\n')
-  os.system('clear')
-  print(red + 'eClear')
-  print(green +'Cleans the console memory and everything currenty on it.')
-  print()
-  print(red + 'eTime')
-  print(green + 'Will make the computer wait a select amount of time')
-  print()
-  print(red + 'eTypewriter')
-  print(green + 'Will make the typewriter effect happen to any of the text you input')
-  print()
-  print(red + 'ePython')
-  print('Will teach you the basics of python!')
-	
+def Elist():
+  if a == "eList":
+    os.system('clear')
+    typewriter('Here are the commands!\n')
+    os.system('clear')
+    print(red + 'eClear')
+    print(green +'Cleans the console memory and everything currenty on it.')
+    print()
+    print(red + 'eTime')
+    print(green + 'Will make the computer wait a select amount of time')
+    print()
+    print(red + 'eTypewriter')
+    print(green + 'Will make the typewriter effect happen to any of the text you input')
+    print()
+    print(red + 'ePython')
+    print(green + 'Will teach you the basics of python!')
+    print()
+  print(red+ 'ePrint')
+  print(green + 'Will print any text that you input')
+  
+
+
+
+
+  if a == "eClear":
+    os.system('clear') 
+    del a, black, red, green, yellow, blue, magenta, cyan, white, bblack
+
   
 
 	
