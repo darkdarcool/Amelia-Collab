f4573d6f7f70ae9d88ed96d9c46344cdf56ac9d8
Darren Reddy <66882633+darkdarcool@users.noreply.github.com>
2020-10-17 17:48:31 -0700

    Update python.py
    Added basics

--- python.py
+++ python.py
@@ -25,4 +25,32 @@ bmagenta = "\033[0;95m"
 bcyan = "\033[0;96m"
 bwhite = "\033[0;97m"
 def python():
+	  os.system('clear')
+    typewriter('Here are your choices for classes\n')
+    print('a) Beginner')
+    print('b) Intermedate')
+    classe = input('')
+    if classe == "a":
+       os.system('clear')
+       typewriter('Welcome beginners! Here are a few classes that are availible! ')
+       print(red + 'a) print function')
+       print()
+       print('b) Variables')
+       print()
+       print('c) In print functions')
+       print()
+       print('d) If, elif')
+       py = input('Which course?').lowwer()
+       py = py.lowwer()
+       if py == "a"
+	    typewriter(green + 'Hi! This is me! Zoom bot! Miss me! Well, I\'m going to assume, that you have no idea how to to anything with python!\nIt is one of the easiest languages to learn! It also capable of so much! From typing hello world, to creating discord bots and Nuraeul Networks! ')
+	    time.sleep(2)
+	    os.system('clear')
+	    typewriter('So, why don\'t we get started!')
+	    time.sleep(2)
+	    os.system('clear')
+	    
+    elif classe == "b":
+      typewriter('Hello fellow coders!')
+      print(red + 'Imports')
   
